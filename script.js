b5b04aaa7b6083b9255acef52b4d19df1a0659db
Ten Cua Ban <email_github_cua_ban@gmail.com>
2026-09-12 03:10:00 +0700

    hoan thien code linkbio

--- script.js
+++ script.js
@@ -42,8 +42,9 @@ function renderProducts() {
         const cardClass = isReverse ? 'product-card reverse' : 'product-card';
 
         // Biến trực tiếp khung sản phẩm thành thẻ a để bấm mượt mà
+    // Thêm hiệu ứng active: khi nhấn vào (touchstart / mousedown) sẽ bị thu nhỏ nhẹ (scale 0.97)
         html += `
-            <a href="${item.link}" target="_blank" id="card-${item.id}" class="${cardClass}" data-id="${item.id}" style="text-decoration: none; -webkit-tap-highlight-color: transparent;">
+            <a href="${item.link}" target="_blank" id="card-${item.id}" class="${cardClass}" data-id="${item.id}" style="text-decoration: none; -webkit-tap-highlight-color: transparent; transition: transform 0.1s ease;" onmousedown="this.style.transform='scale(0.97)'" onmouseup="this.style.transform='scale(1)'" onmouseleave="this.style.transform='scale(1)'" ontouchstart="this.style.transform='scale(0.97)'" ontouchend="this.style.transform='scale(1)'">
                 <!-- Nửa ảnh (50%) -->
                 <div class="card-image">
                     <img src="${item.image}" alt="Mã ${item.id}">
